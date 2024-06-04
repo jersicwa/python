601adc0ce32d5036b96d1a40c5342f70828ed74f
jessica <jessboll16@gmail.com>
2024-06-04 14:02:57 -0500

    yo mom

---
+++ turtlefun.py
@@ -0,0 +1,16 @@
+import turtledemo
+
+t = turtle.Turtle()
+t.shape('turtle')
+for i in range(4):
+  for i in range(1):
+    t.forward(100)
+    t.rt(90)
+  for i in range(4):
+    t.rt(-90)
+    t.forward(20)
+# All of your turtle commands
+# go in this space here.
+
+
+turtle.mainloop()      # should be last line of program
